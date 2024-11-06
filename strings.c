31a43fad9b8f37a042f71d76c73025e28b64e344
vigneshperiasamy <darkespyt8577@gmail.com>
2024-11-06 12:35:39 +0530

    Strings added

---
+++ strings.c
@@ -0,0 +1,123 @@
+/system/bin/linker64
+Android
+4754217
+liblog.so
+libz.so
+libc.so
+closedir
+fopen
+fgets
+__stack_chk_fail
+opendir
+readdir
+snprintf
+__libc_init
+__errno
+sscanf
+sprintf
+__cxa_atexit
+perror
+popen
+strcmp
+syscall
+strstr
+fclose
+pclose
+atof
+atoi
+libm.so
+libdl.so
+_edata
+__bss_start
+_end
+__PREINIT_ARRAY__
+__FINI_ARRAY__
+__INIT_ARRAY__
+LIBC
+ptrace attach
+ptrace detach
+Write error
+errno: %d
+/proc/%d/maps
+%lx-%*s
+pidof %s
+/proc
+/proc/%s/cmdline
+[30;1m
+[31;1m
+[32;1m
+[33;1m
+[34;1m
+[35;1m
+[36;1m
+[37;1m
+su -c %s
+FANSPRO
+%lx-%lx
+/dev/kgsl-3d0
+[anon:libc_malloc]
+[anon:.bss]
+/data/app/
+[heap]
+/dev/ashmem/
+rw-p
+/system
+[stack]
+[anon:thread signal stack]
+dalvik
+r-xp
+addr:0x%lX,taddr:0x%lX
+map error
+search error
+can not get pid
+/proc/%d/mem
+RangeSearch Error
+offset error
+RangeOffset error
+/data/data/%s/
+/proc/
+pm uninstall %s
+pm install %s
+kill %d
+/proc/%d/status
+State
+State: %c
+su -c 'reboot'
+date +%F-%T
+date +%F-%T > log.txt
+/data/data
+/data/data/%s/files
+/data/data/%s/lib/libxscript.so
+Error -1
+Error -2
+You Choose a NULL type
+FreezeAddr:%lx,type:%s,value:%s
+QWORD
+DWORD
+com.pubg.imobile
+libUE4.so
+GCC: (GNU) 4.9.x 20150123 (prerelease)
+clang version 7.0.0 
+.shstrtab
+.interp
+.note.android.ident
+.note.gnu.build-id
+.hash
+.dynsym
+.dynstr
+.gnu.version
+.gnu.version_r
+.rela.dyn
+.rela.plt
+.text
+.rodata
+.eh_frame_hdr
+.eh_frame
+.preinit_array
+.init_array
+.fini_array
+.dynamic
+.got
+.data
+.bss
+.comment
